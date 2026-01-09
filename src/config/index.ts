85954b80567cbf6dcaa667d175c505f188679906
Omar Faruk Sarkar <43296805+frksarkar@users.noreply.github.com>
2026-01-09 21:29:14 +0600

    feat: add index files for config, controllers, middleware, models, routes, and utils

---
+++ src/config/index.ts
@@ -0,0 +1,2 @@
+export { default as connectDB } from './db.config';
+export { default as swaggerSpec } from './swagger.config';
---
+++ src/routes/index.ts
@@ -0,0 +1,2 @@
+export { default as authRoutes } from './auth.route';
+export { default as urlRoutes } from './url.route';
