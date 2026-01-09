201b7aa79bea7c9b474678c6c6f6b7e5bfced7c7
Omar Faruk Sarkar <43296805+frksarkar@users.noreply.github.com>
2026-01-09 22:31:21 +0600

    refactor: streamline user authentication logic in auth.controller

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express';
-import User from '../models/User.model';
-import { generateToken } from '../utils/jwt.utils';
+import { User } from '../models';
+import { generateToken } from '../utils';
 
 // @desc    Register user
 // @route   POST /api/auth/register
@@ -12,7 +12,6 @@ export const register = async (req: Request, res: Response) => {
 		if (!name || !email || !password) {
 			return res.status(400).json({ message: 'All fields are required' });
 		}
-
 		const userExists = await User.findOne({ email });
 		if (userExists) {
 			return res.status(400).json({ message: 'User already exists' });
@@ -39,26 +38,25 @@ export const register = async (req: Request, res: Response) => {
 export const login = async (req: Request, res: Response) => {
 	try {
 		const { email, password } = req.body;
-		console.log(email, password);
-		return res.status(200).json({ message: 'Login successful' });
-		// const user = await User.findOne({ email });
-		// if (!user) {
-		// 	return res.status(400).json({ message: 'Invalid credentials' });
-		// }
 
-		// const isMatch = await (user as any).comparePassword(password);
-		// if (!isMatch) {
-		// 	return res.status(400).json({ message: 'Invalid credentials' });
-		// }
+		const user = await User.findOne({ email });
+		if (!user) {
+			return res.status(400).json({ message: 'Invalid credentials' });
+		}
 
-		// const token = generateToken(user._id.toString());
+		const isMatch = await (user as any).comparePassword(password);
+		if (!isMatch) {
+			return res.status(400).json({ message: 'Invalid credentials' });
+		}
 
-		// res.json({
-		// 	_id: user._id,
-		// 	name: user.name,
-		// 	email: user.email,
-		// 	token,
-		// });
+		const token = generateToken(user._id.toString());
+
+		res.json({
+			_id: user._id,
+			name: user.name,
+			email: user.email,
+			token,
+		});
 	} catch (error) {
 		console.error(error);
 		res.status(500).json({ message: 'Server error' });
